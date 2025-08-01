e526652a4e2dae3571d186c1d5fd42c529a90e76
Reuben <reubenamuzu23@gmail.com>
2025-08-01 14:55:31 +0000

    Hey

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -19,7 +19,7 @@ const Intro = () => {
                 <div className="introContentContainer"> {/* New wrapper for flex layout */}
                     <div className="introTextContent"> {/* Wrapper for all text and button */}
                         <span className="hello">Hello,</span>
-                        <span className="introText">I'm <span className="introName">Reuben</span> <br/> <span className="introHead"> <div> A Software Developer </div> and Mobile App Builder </span> </span>
+                        <span className="introText">I'm <span className="introName">Reuben</span> <br/> <span className="introHead">  A Software Developer and Mobile App Builder </span> </span>
                         <p className="introPara">I create modern, user-focused apps and websites  <br /> that are fast, responsive, and beautifully designed. </p>
 
                         <a href="https://drive.google.com/file/d/1t8PRdyFjlNcYzlhA-Igtp9WdtwZ3x5vX/view?usp=sharing" target="_blank" rel="noopener noreferrer">
